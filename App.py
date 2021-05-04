8edb9c6fbdef465c1693e22e5e5504103b89b0ca
Vojta188 <vojtechvrabec12@gmail.com>
2021-05-04 15:09:25 +0200

    přidání while

--- App.py
+++ App.py
@@ -1,17 +1,39 @@
-stav_uctu = float(input('Zadej stav tvého účtu:'))
-mesicni_prijem = float(input('Zadej příjem za tento měsíc:'))
-mesicni_najem = float(input('Zadej měsíční nájem:'))
-mesicne_poplatky = float(input('Zadej ostatní poplatky za měsíc:'))
-dluhy = float(input('Zadej součet dlužných částek:'))
-utrata_jidlo = float(input('Denní útrata na jídlo:'))
 
-zbytek = mesicni_prijem - mesicni_najem - mesicne_poplatky - dluhy
-utrata_jidlo_mesic = 30 * utrata_jidlo
+while True:
+    konec = input('Jestli chceš ukončit program napiš konec. V opačném případě stiskni enter.')
+    if konec == 'konec':
+        print("program byl ukončen")
+        break
 
-print('stav tvého účtu:',stav_uctu)
-print('Příjem tento měsíc:',mesicni_prijem)
-print('Měsíčně po zaplacení poplatku zbyde', zbytek)
-print('Měsíční útrata za jídlo:', utrata_jidlo_mesic)
+    stav_uctu = float(input('Zadej stav tvého účtu:'))
+    mesicni_prijem = float(input('Zadej příjem za tento měsíc:'))
+    mesicni_najem = float(input('Zadej měsíční nájem:'))
+    mesicne_poplatky = float(input('Zadej ostatní poplatky za měsíc:'))
+    dluhy = float(input('Zadej součet dlužných částek:'))
+    utrata_jidlo = float(input('Denní útrata na jídlo:'))
+
+    zbytek = mesicni_prijem - mesicni_najem - mesicne_poplatky - dluhy
+    utrata_jidlo_mesic = 30 * utrata_jidlo
+    kompletni_utrata = mesicni_najem + mesicne_poplatky + utrata_jidlo_mesic + dluhy
+    konicky = mesicni_prijem - kompletni_utrata
+    porovnani = konicky <= 0
+
+    print('stav tvého účtu:',stav_uctu)
+    print('Příjem tento měsíc:',mesicni_prijem)
+    print('Měsíčně po zaplacení poplatku zbyde', zbytek)
+    print('Měsíční útrata za jídlo:', utrata_jidlo_mesic)
+    print('Kompletní útrata za Jídlo, Nájem, Poplatky:', kompletni_utrata)
+    print('Peníze pro osobní potřebu:', konicky)
 
 
+    if porovnani:
+        {
+        print("Špatný měsíc")
+        }
+    else:
+        {
+        print("Dobrý měsíc")
+        }
 
+
+print("Hezký den")
